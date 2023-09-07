4859814386adffc0b275bdd53da710b833097262
nachaal <144372378+nachaal@users.noreply.github.com>
2023-09-07 22:50:48 +0530

    Add files via upload

---
+++ challenges 1/main (1).py
@@ -0,0 +1,14 @@
+1.2 Write the python program to check whether the year is leap year or not
+def isLeapyear(year):
+   if(year%4==0 and year%100!=0) or
+  year%400==0:
+       return True
+     else
+       return False
+year=int(input("Enter a year"))
+if isLeapayear(year):
+  print("{} is a leap year:".format(year))
+else:
+  print("{} is not a leap
+  year!".format(year))
+
