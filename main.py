f75216f9222fb6bc384937dad4bb4666eebc346a
nachaal <144372378+nachaal@users.noreply.github.com>
2023-09-23 21:44:17 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -1,40 +1,15 @@
-#write the program to create a instance of BankAccount class and test the deposit and withdrawal functionality.
-class BankAccount:
-
-  def __init__(self, account_number, account_holder_name, initial_balance=0.0):
-    self.__account_number = account_number
-    self.__account_holder_name = account_holder_name
-    self.__account_balance = initial_balance
-
-  def deposit(self, amount):
-    if amount > 0:
-      self.__account_balance += amount
-      print("New balance:₹.{}".format(amount, self.__account_balance))
-    else:
-      print("Invalid deposit amount. Please deposit a positive amount.")
-
-  def withdraw(self, amount):
-    if amount > 0 and amount <= self.__account_balance:
-      self.__account_balance -= amount
-      print("Withdrawl ₹.{}. New balance: ₹.{}".format(amount,
-                                                       self.__account_balance))
-    else:
-      print("Invalid withdrawal amount or insufficient balance.")
-
-  def display_balance(self):
-    print("Account balance for {} (Account#{}):₹{}".format(
-        self.__account_holder_name, self.__account_number,
-        self.__account_balance))
+#Write a function called linear_search_product that takes the list of products and a target product name as input.
+def linear_search_product(productList, targetProduct):
+  indices = []
+  for index, product in enumerate(productList):
+    if product == targetProduct:
+      indices.append(index)
+  return indices
 
 
-#create an instance of the BankAccount class:
-account = BankAccount(account_number="1234567890",
-                      account_holder_name="Mahalakshmi Mohan",
-                      initial_balance=7000.00)
-
-#test deposits and withdrawal functionality:
-account.display_balance()
-account.deposit(500.0)
-account.withdraw(200.0)
-account.withdraw(20000.0)
-#account.display_balance()
+#Example usage:
+products = ["shoes", "boot", "loafer", "shoes", "sandal", "shoes"]
+target = "shoes"
+target2 = "apple"
+result = linear_search_product(products, target)
+print(result)
